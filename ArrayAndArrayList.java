67d08660f9b989ef1a78d0b438b62b361b7ac157
Yashrajpawar-dev <yashrajpawar9604@gmail.com>
2025-07-08 22:05:44 +0530

    Add ArrayAndArrayList and Methods classes with basic functionality

---
+++ ArrayAndArrayList.java
@@ -0,0 +1,18 @@
+public class ArrayAndArrayList {
+    public static void main(String[] args) {
+    
+// Q: store a roll no
+    int rollno = 5;
+
+    // Q: store a persons name
+    String name = "yash";
+
+    // Q: store 5 roll numbers
+    int rollno1 = 29;
+    int rollno2 = 61;
+    int rollno3 = 44;
+
+    //Syntax
+    // datatype[] variable_name = new datatype[size];
+    }
+}
