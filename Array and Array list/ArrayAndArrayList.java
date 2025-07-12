eb5d15e22e87d19a58515618c5ba355851a6e87e
Yashrajpawar-dev <yashrajpawar9604@gmail.com>
2025-07-12 18:37:53 +0530

    Add clarification comment about java.util imports in ArrayAndArrayList.java

--- Array and Array list/ArrayAndArrayList.java
+++ Array and Array list/ArrayAndArrayList.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+// All the things can found in java.util.*;
+
 public class ArrayAndArrayList {
     public static void main(String[] args) {
     
