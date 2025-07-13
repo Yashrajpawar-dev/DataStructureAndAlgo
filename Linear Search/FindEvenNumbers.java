3b177b117e107766534af84b87a818cb0912cdba
Yashrajpawar-dev <yashrajpawar9604@gmail.com>
2025-07-13 09:12:14 +0530

    Add FindEvenNumbers.java to implement functionality for counting numbers with even digits

---
+++ Linear Search/FindEvenNumbers.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class FindEvenNumbers {
+
+    //https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+    public static void main(String[] args) {
+        
+         int arr[] = {768, 997, 293, 471, 120, 414, 987, 13, 5988,};
+         System.out.println(solution(arr));
+    } 
+    
+    static int solution(int[] arr){
+  
+
+         int count = 0;
+
+         for(int i = 0 ; i< arr.length;i++){
+            if(even(arr[i])){
+                count++;
+            }
+         }
+         return count;
+
+        
+        }
+
+static boolean even(int num){
+    int number = digits(num);
+    if (number % 2 ==0 ) {
+        return true;
+        
+    }
+    return false;
+}
+static int digits(int num){
+    int count = 0; //  to check the number is even or odd
+    
+   while(num>0){
+    count++;
+    num = num/10;
+
+   }
+return count;
+}
+}
